7e150aa521d05efd948270f7003897c66a28d947
ehcalmdown <96929724+ehcalmdown@users.noreply.github.com>
2022-07-20 10:27:39 +0800

    modified day13workshop

--- src/main/java/sg/edu/nus/iss/day13workshop/Day13workshopApplication.java
+++ src/main/java/sg/edu/nus/iss/day13workshop/Day13workshopApplication.java
@@ -1,13 +1,25 @@
 package sg.edu.nus.iss.day13workshop;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class Day13workshopApplication {
+public class Day13workshopApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Day13workshopApplication.class, args);
 	}
 
+	@Override
+	public void run (ApplicationArguments args){
+		if(args.containsOption("dataDir")){
+			final String dataDir = args.getOptionValues("dataDir").get(0);
+			System.out.printf("Using %s as data directory \n", dataDir);
+		}else {
+
+		}
+	}
 }
